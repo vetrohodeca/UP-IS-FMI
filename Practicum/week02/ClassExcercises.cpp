3e25ab9ef61996ff6203c26a5f37d6238cc54da4
Fill99 <aleksandarfilisyan@abv.bg>
2019-10-28 09:30:41 +0200

    Add week03 tasks solutions

--- Practicum/week02/ClassExcercises.cpp
+++ Practicum/week02/ClassExcercises.cpp
@@ -64,7 +64,7 @@ int main()
 	int year;
 	bool isLeap=false;
 	cin>>year;
-	if(year%4==0&&year%100==0&&year%400!=0)
+	if(year%4==0&&year%100==0&&year%400==0)
 	{
 		isLeap=true;		
 	}
@@ -108,15 +108,191 @@ int main()
 			cout<<"No solution";
 			return 0;
 		}
-		*/
+		
 		
 		//6 task
-		int number;
+	/*	int number;
 		int digit;
 		cin>>number;
 		cin>>digit;
 		bool flag=0;
-		if(number)
+		if(number%10==digit|| number/10%10==digit||number/100%10==digit||number/1000%10==digit||number/10000%10==digit)
+		{
+			flag=true;
+		}
+		cout<<boolalpha<<flag;
+		*/
+		
+		//7 task
+	/*	int number;
+		cin>>number;
+		switch (number)
+		{
+			case 1:
+			cout<<"winter"<<endl;break;
+		case 2: 
+			cout<<"winter"<<endl;break;
+		case 3:
+			cout<<"winter"<<endl;break;
+		case 4:
+			cout<<"spring"<<endl;break;
+		case 5:
+			cout<<"spring"<<endl;break;
+		case 6:
+			cout<<"spring"<<endl;break;
+		case 7:
+			cout<<"summer"<<endl;break;
+		case 8: 
+			cout<<"summer"<<endl;break;
+		case 9: 
+			cout<<"summer"<<endl;break;
+		case 10:
+			cout<<"winter"<<endl;break;		
+		case 11: 
+			cout<<"winter"<<endl;break;		
+		case 12:
+			cout<<"winter"<<endl;break;
+		default:
+			cout<<"There is no such month"<<endl;
+		}		
+		*/
+		
+		//8 task
+		/*	int number;
+		cin>>number;
+		switch (number)
+		{
+			case 1:
+			cout<<"31"<<endl;break;
+		case 2: 
+			cout<<"28"<<endl;break;
+		case 3:
+			cout<<"31"<<endl;break;
+		case 4:
+			cout<<"30"<<endl;break;
+		case 5:
+			cout<<"31"<<endl;break;
+		case 6:
+			cout<<"30"<<endl;break;
+		case 7:
+			cout<<"31"<<endl;break;
+		case 8: 
+			cout<<"31"<<endl;break;
+		case 9: 
+			cout<<"30"<<endl;break;
+		case 10:
+			cout<<"31"<<endl;break;		
+		case 11: 
+			cout<<"30"<<endl;break;		
+		case 12:
+			cout<<"31"<<endl;break;
+		default:
+			cout<<"There is no such month"<<endl;
+		}
+		*/
+		/*
+		char symbol;
+		int digit1;
+		int digit2;
+		cin>>symbol>>digit1>>digit2;
+		if(symbol=='+')
+		{
+			cout<<digit1+digit2<<endl;
+		}
+		else if(symbol=='-')
+		{
+			cout<<digit1-digit2<<endl;
+		}
+		else if(symbol=='*')
+		{
+			cout<<digit1*digit2<<endl;
+		}
+		else if(symbol=='/')
+		{
+			cout<<digit1/digit2<<endl;
+		}
+		else if(symbol=='%')
+		{
+			cout<<digit1%digit2;
+		}
+		else 
+		{
+			cout<<"Invalid operator";
+		}
+		*/
+		
+		//10 task
+		/*
+		char symbol;
+		cin>>symbol;
+		if(symbol>=48&&symbol<=57)
+		{
+			cout<<(int)(symbol-48)*(int)(symbol-48);
+		}
+		else if(symbol>=97&&symbol<=122)
+		{
+			cout<<(char)(symbol-32);
+		}*/
+		//11 task
+		
+		int month;
+		int day;
+		cin>>day>>month;
+		int remeaningDays;
+		if(day<=0||day>31)
+		{
+			cout<<"There is no such day";
+			return 0;
+		}
+		if(month<=0||month>12)
+		{
+			cout<<"There is no such month";
+			return 0;
+		}
+		int daysInMonth;
+		switch (month)
+		{
+			case 1:
+				remeaningDays=335;
+				daysInMonth=31;break;
+				
+			case 2:
+				daysInMonth=29;
+				remeaningDays=306;break;				
+			case 3:
+				daysInMonth=31;
+				remeaningDays=275;break;
+			case 4:
+				remeaningDays=245;
+				daysInMonth=30;break;
+			case 5:
+				remeaningDays=214;
+				daysInMonth=31;break;
+			case 6:
+				remeaningDays=183;
+				daysInMonth=30;break;
+			case 7:
+				remeaningDays=153;
+				daysInMonth=31;break;
+			case 8:
+				remeaningDays=122;
+				daysInMonth=31;break;
+			case 9:
+				remeaningDays=92;
+				daysInMonth=30;break;
+			case 10:
+				remeaningDays=61;
+				daysInMonth=31;break;
+			case 11:
+				daysInMonth=30;
+				remeaningDays=31;break;
+			case 12:
+				remeaningDays=0;
+				daysInMonth=31;break;
+		}
+		remeaningDays+=daysInMonth-day;
+		        cout<<remeaningDays<<endl;
+		
 	return 0;
 }
 
