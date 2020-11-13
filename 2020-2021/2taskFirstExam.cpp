cf0d44137143e57bb76384801afb369c94291382
Fill99 <aleksandarfilisyan@abv.bg>
2020-11-13 18:05:10 +0200

    add tasks

---
+++ 2020-2021/2taskFirstExam.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	/*int n;
+	int counter=0;
+	cin>>n;
+	while(n!=1)
+	{
+		if(n%2==0)
+		{
+			n/=2;
+		}
+		else
+		{
+			n=3*n+1;
+		}
+		counter++;
+	}*/
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		
+	}
+	cout<<counter;
+	return 0;
+}
